75d0dbf399fba4b324c9e470f0160e3c1c19b277
MostafaAhmedSabry <mostafaahmedsabryaboseif@gmail.com>
2025-10-26 23:48:53 +0300

    first commit

---
+++ lib/view/login/new_password_view.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+import '../../extension/color_extension.dart';
+import '../../widget/round_button.dart';
+import '../../widget/round_textfield.dart';
+
+
+class NewPasswordView extends StatefulWidget {
+  final Map nObj;
+  const NewPasswordView({super.key, required this.nObj});
+
+  @override
+  State<NewPasswordView> createState() => _NewPasswordViewState();
+}
+
+class _NewPasswordViewState extends State<NewPasswordView> {
+
+  TextEditingController txtPassword = TextEditingController();
+  TextEditingController txtConfirmPassword = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 64,
+              ),
+              Text(
+                "New Password",
+                style: TextStyle(
+                    color: TColor.primaryText,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Text(
+                "Please enter your new password",
+                style: TextStyle(
+                    color: TColor.secondaryText,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              RoundTextfield(
+                hintText: "New Password",
+                controller: txtPassword,
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              RoundTextfield(
+                hintText: "Confirm Password",
+                controller: txtConfirmPassword,
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              RoundButton(title: "Next", onPressed: () {
+                // btnSubmit();
+              }),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
